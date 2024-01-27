ad4ac45fb2126bfd581e28eafc928b210bcd09e3
Yevhen Tsapko <evgentsa@gmail.com>
2024-01-27 14:23:21 +0200

    restaurant details is made

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,8 +1,21 @@
 $(document).ready(function () {
-  $('.slider').slick({
+  $('.slider-about-rest').slick({
     infinite: true,
     slidesToShow: 2,
     slidesToScroll: 1,
-    autoplay: true,
+    appendArrows: $('.slider-about-rest'),
+    prevArrow: $('.prev-arrow.about-rest'),
+    nextArrow: $('.next-arrow.about-rest'),
+  });
+});
+$(document).ready(function () {
+  $('.slider-rest-details').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    appendArrows: $('.slider-rest-details'),
+    prevArrow: $('.prev-arrow.rest-details'),
+    nextArrow: $('.next-arrow.rest-details'),
+    variableWidth: true,
   });
 });
