23ac7cb68647e3c49f6295d85e9934631922870f
Yevhen Tsapko <evgentsa@gmail.com>
2024-02-12 15:32:41 +0200

    fix slider

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 // import '//code.jquery.com/jquery-1.11.0.min.js';
 // import '//code.jquery.com/jquery-migrate-1.2.1.min.js';
-import './js/slick.min.js';
+// import './js/slick.min.js';
 import './js/slick.js';
